6f27412235c3f25992d342b46a6d7b83a84a3839
ujwal <shanbhagujwal@gmail.com>
2026-01-22 20:24:32 +0530

    project structure ready

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="src",
+    version="0.0.1",
+    author="ujwal3110",
+    author_email="shanbhagujwal@gmail.com",
+    packages=find_packages()
+)
